b84a24abd875626e762ead96f0dd0c0004dd9401
Shruti <shrutisablaniya93@gmail.com>
2021-09-27 16:30:37 +0530

    Maximum value in Array

---
+++ UC4_MaximumInArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMaximum
+{
+    class UC4_MaximumInArray
+    {
+        public static int MaximumInteger(int[] arr)
+        {
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+                if (arr[i] > max)
+                    max = arr[i];
+            return max;
+        }
+
+        public static float MaximumFloat(float[] arr)
+        {
+            float max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+                if (arr[i] > max)
+                    max = arr[i];
+            return max;
+        }
+
+        public static String MaximumString(List<string> arr)
+        {
+            arr.Sort();
+            string max = arr[arr.Count - 1];
+            return max;
+        }
+    }
+}
